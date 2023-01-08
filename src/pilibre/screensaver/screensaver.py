ade3f6dff0052a57b9f51b6d5cf01b688735c00f
acbuie <aidancbuie@gmail.com>
2023-01-08 11:04:03 -0700

    docs: add docstrings to screensaver functions

--- src/pilibre/screensaver/screensaver.py
+++ src/pilibre/screensaver/screensaver.py
@@ -14,6 +14,16 @@ from pilibre.screensaver._logo import LOGO
 
 
 def create_screen(logo: Text, console: Console) -> Layout:
+    """Creates a static screensaver.
+
+    Args:
+        logo (Text): Text to display.
+        console (Console): Console instance.
+
+    Returns:
+        Layout: Screensaver to display.
+    """
+
     date_time = format_datetime_title("America/Denver")
 
     aligned_logo = align_logo(logo, console.width, console.height)
@@ -31,8 +41,16 @@ def create_screen(logo: Text, console: Console) -> Layout:
     return screen
 
 
-def run_screensaver() -> None:
+def run_screensaver(seconds: int) -> None:
+    """Runs the screensaver, for the supplied amount of seconds.
+
+    Args:
+        seconds (int): Time to run screensaver, in seconds.
+
+    Returns:
+        None
+    """
     with Live(create_screen(LOGO, console), refresh_per_second=4) as live:
-        for _ in range(10):
+        for _ in range(seconds):
             time.sleep(1)
             live.update(create_screen(LOGO, console))
