5ef4fe1efc751458164d08ec90e1b1a596a6ae18
acbuie <aidancbuie@gmail.com>
2023-01-16 20:22:44 -0700

    feat: pad meter to the end of line with dots

--- src/pilibre/ui/components/meter.py
+++ src/pilibre/ui/components/meter.py
@@ -3,8 +3,8 @@
 import random  # TODO: placeholder data
 
 from rich.console import Console, ConsoleOptions, RenderResult
-from rich.segment import Segment
 from rich.style import Style
+from rich.text import Text
 
 from pilibre._color import color_bar_colors
 from pilibre.config.themes import TERMINAL_THEME
@@ -31,19 +31,28 @@ class Meter:
         self, console: Console, options: ConsoleOptions
     ) -> RenderResult:
 
-        width = options.max_width
+        width = options.max_width - 2
 
         colors = color_bar_colors(self.low, self.high, width, TERMINAL_THEME)
 
         percentage = int(width * (self.usage / 100.0))
-        chopped_colors = colors[0:percentage]
+        used_colors = colors[0:percentage]
 
-        for color in chopped_colors:
-            yield Segment("\u2588", Style(color=color))
+        background = Text("·" * width)
+
+        # Alternative chars: ■, █,
+
+        meter_chars = Text()
+        for color in used_colors:
+            meter_chars.append_text(Text("■", Style(color=color)))
+
+        padded_meter = meter_chars.append_text(background[percentage:])
+
+        yield Text("⟨") + padded_meter + Text("⟩")
 
 
 def generate_meter() -> Meter:
-    """Wrapped for updating a `Meter` with `Rich.Live`."""
+    """Wrapper for updating a `Meter` with `Rich.Live`."""
     meter = Meter("bright_white", "red")
 
     usage = random.randint(0, 100)  # TODO: placeholder data
@@ -51,7 +60,8 @@ def generate_meter() -> Meter:
 
     return meter
 
-    # with Live(generate_meter(), console=console, refresh_per_second=4) as live:
-    #     for _ in range(20):
-    #         time.sleep(0.5)
-    #         live.update(generate_meter())
+
+# with Live(generate_meter(), console=console, refresh_per_second=4) as live:
+#     for _ in range(20):
+#         time.sleep(0.5)
+#         live.update(generate_meter())
